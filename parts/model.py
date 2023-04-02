575eaf051ae1c7eb871c34ca69c02b64d3b75e46
Colton Gray <78562164+GrayoftheGalaxy@users.noreply.github.com>
2023-04-02 16:49:16 -0500

    Add files via upload
    Added saveCreation() function, saves creations to a folder called "SavedCreations" in the parts directory

--- parts/model.py
+++ parts/model.py
@@ -1,4 +1,5 @@
-import PIL
+#import PIL
+import os
 
 
 # This same model will be used in play and create modes. In play mode, users will update the play_grid
@@ -6,7 +7,6 @@ import PIL
 # update the play grid until they are satisfied. Each grid willcontain integers values that each
 # correspond to an indice in the colors array. The colors array will just be an array of hex values
 # as strings
-
 class Model:
     def __init__(self, type, colors, width, height):
         self.colors = []
@@ -16,7 +16,7 @@ class Model:
             self.filled_grid = self.generate_filled_grid()
         if type == 'fill_from_img':
             self.filled_grid = self.generate_filled_grid_from_img()
-        if type == 'create_from_scratch':
+        if type == 'create_from_scratch': 
             self.play_grid = [[-1] * width] * height
 
     def add_color(self, color):
@@ -39,8 +39,24 @@ class Model:
     def generate_filled_grid_from_img():
         pass
 
+    #Saves the current displayed image to a file to be used later, asks for a name when saving
+    #Example:
+    ##FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF |;#FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF | #FFFFF |
+    # This is two rows of 10 colors, thr rows being seperated by a ;
+    def saveCreation(name):
+        name = name + ".txt"
+        filePath = os.getcwd() + "\SavedCreations" + '\\' + name
+        file = open(filePath, "w") #Overwrites previous creation if it already existed
+        
+        for row in colors: #Saves all colors to file, seperates row members with a | and rows with a ;.
+            for color in colors:
+                file.write(color + " | ")
+            file.write(";")
 
+        file.close()
+        
     # Files will be structured as follow:
     # For reading these, save the entire thing as a string, then use split on ":" and then split on the "|". Need to make sure the grid actually contains numbers and not strings as w3ll.
     #  colors : 1st array of colors | 2nd array of colors | ............. | nth array of colors
     #  #FFFFFF | #FFFFFF | #FFFFFF | #FFFFFF : 1 1 1 1 1 1 1 1 1 1 | 1 1 1 1 1 1 1 1 1 1 | ................. | 1 1 1 1 1 1 1 1 1 1
+    
